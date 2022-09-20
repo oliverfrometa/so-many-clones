5093ed27b2b3c83f04e46a6c517b7b372ebc18c9
Oliver Frometa <oliverfrometa17@gmail.com>
2022-09-19 22:57:22 -0400

    clean / swap / add

---
+++ clones/humanrace/app.js
@@ -0,0 +1,14 @@
+const aboutHumanraceBtn = document.querySelector('.about-humanrace');
+const aboutHumanraceContainer = document.querySelector('.about-humanrace-container')
+
+// aboutHumanraceContainer.style.display = "none";
+
+// if (aboutHumanraceContainer.style.display = 'none') {
+//     aboutHumanraceBtn.addEventListener('mouseover', () => {
+//     aboutHumanraceContainer.style.display = "flex";
+// })
+// } if (aboutHumanraceContainer.style.display = "flex") {
+//     aboutHumanraceBtn.addEventListener('mouseout', () => {
+//     aboutHumanraceContainer.style.display = "none";
+// })
+// }
