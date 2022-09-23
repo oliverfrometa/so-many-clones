d2f1158844b1a77d588590f94a4d24b1e2cda69f
Oliver Frometa <oliverfrometa17@gmail.com>
2022-09-23 13:26:10 -0400

    nav-bar-update

--- clones/humanrace/app.js
+++ clones/humanrace/app.js
@@ -1,5 +1,4 @@
 const navBackground = document.querySelector("nav")
-
 window.onscroll = () => {
     let top = window.scrollY;
     if (top >= 50) {
@@ -11,7 +10,6 @@ window.onscroll = () => {
 
 const ahrBtn = document.querySelector(".about-humanrace")
 const ahrContainer = document.querySelector(".about-humanrace-container")
-
 ahrBtn.addEventListener("mouseover", () => {
     ahrContainer.classList.add("active")
     navBackground.classList.add("active")
@@ -27,15 +25,122 @@ ahrBtn.addEventListener("mouseover", () => {
 const burger = document.querySelector(".burger")
 const mobileMenu = document.querySelector("#mobile-menu")
 const setsandkits = document.querySelector("#setsandkits")
-
-
-let toggleStatus = 1;
+let toggleMenuStatus = true;
 function toggleMenu() {
-    if (toggleStatus == 1) { 
+    if (toggleMenuStatus == true) { 
         mobileMenu.style.display = "flex"
-         toggleStatus = 0;
-    } else if (toggleStatus == 0) {
-        toggleStatus = 1;
+         toggleMenuStatus = false;
+    } else if (toggleMenuStatus == false) {
+        toggleMenuStatus = true;
         mobileMenu.style.display = "none"
+    }
+}
+
+
+// ------------------------------------------ MOBILE MENU
+
+// const setsandkitsBtn = document.querySelector("#setsandkits-btn")
+// const skincareBtn = document.querySelector("#skincare-btn")
+// const bodycareBtn = document.querySelector("#bodycare-btn")
+// const adidasBtn = document.querySelector("#adidas-btn")
+// const ceramicsBtn = document.querySelector("#ceramics-btn")
+// const abouthumanraceBtn = document.querySelector("#abouthumanrace-btn")
+
+const setsandkitsSubmenu = document.querySelector("#setsandkits-submenu")
+const skincareSubmenu = document.querySelector("#skincare-submenu")
+const bodycareSubmenu = document.querySelector("#bodycare-submenu")
+const suncareSubmenu = document.querySelector("#suncare-submenu")
+const adidasSubmenu = document.querySelector("#adidas-submenu")
+const ceramicsSubmenu = document.querySelector("#ceramics-submenu")
+const abouthumanraceSubmenu = document.querySelector("#abouthumanrace-submenu")
+
+// sets and kits
+
+let setsandkitsStatus = true;
+function toggleSetsandkits() {
+    if (setsandkitsStatus == true) { 
+        setsandkitsSubmenu.style.display = "flex"
+         setsandkitsStatus = false;
+    } else if (setsandkitsStatus == false) {
+        setsandkitsStatus = true;
+        setsandkitsSubmenu.style.display = "none"
+    }
+}
+
+let skincareStatus = true;
+function toggleSkincare() {
+    if (skincareStatus == true) { 
+        skincareSubmenu.style.display = "flex"
+         skincareStatus = false;
+    } else if (skincareStatus == false) {
+        skincareStatus = true;
+        skincareSubmenu.style.display = "none"
+    }
+}
+
+let bodycareStatus = true;
+function toggleBodycare() {
+    if (bodycareStatus == true) { 
+        bodycareSubmenu.style.display = "flex"
+         bodycareStatus = false;
+    } else if (bodycareStatus == false) {
+        bodycareStatus = true;
+        bodycareSubmenu.style.display = "none"
+    }
+}
+
+let adidasStatus = true;
+function toggleAdidas() {
+    if (adidasStatus == true) { 
+        adidasSubmenu.style.display = "flex"
+         adidasStatus = false;
+    } else if (adidasStatus == false) {
+        adidasStatus = true;
+        adidasSubmenu.style.display = "none"
+    }
+}
+
+let suncareStatus = true;
+function toggleSuncare() {
+    if (suncareStatus == true) { 
+        suncareSubmenu.style.display = "flex"
+         suncareStatus = false;
+    } else if (suncareStatus == false) {
+        suncareStatus = true;
+        suncareSubmenu.style.display = "none"
+    }
+}
+
+let ceramicsStatus = true;
+function toggleCeramics() {
+    if (ceramicsStatus == true) { 
+        ceramicsSubmenu.style.display = "flex"
+         ceramicsStatus = false;
+    } else if (ceramicsStatus == false) {
+        ceramicsStatus = true;
+        ceramicsSubmenu.style.display = "none"
+    }
+}
+
+
+
+// ------------------------------------------
+
+
+const bag = document.querySelector("#bag")
+const bagBtn = document.querySelector("#bag-btn")
+const accountBtn = document.querySelector("#account-btn")
+let toggleBagStatus = true;
+function toggleBag() {
+    if (toggleBagStatus == true) { 
+        bag.style.display = "flex";
+        // bagBtn.style.zIndex = 1000;
+        // accountBtn.style.zIndex = 1000;
+         toggleBagStatus = false;
+    } else if (toggleBagStatus == false) {
+        toggleBagStatus = true;
+        // bagBtn.style.zIndex = 0;
+        // accountBtn.style.zIndex = 0;
+        bag.style.display = "none";
     }
 }
