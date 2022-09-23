95e27ce0b6f196086c2d3bedccf70c6223fe316b
Oliver Frometa <oliverfrometa17@gmail.com>
2022-09-22 22:45:46 -0400

    nav-bar-update

--- clones/humanrace/app.js
+++ clones/humanrace/app.js
@@ -1,14 +1,41 @@
-const aboutHumanraceBtn = document.querySelector('.about-humanrace');
-const aboutHumanraceContainer = document.querySelector('.about-humanrace-container')
+const navBackground = document.querySelector("nav")
 
-// aboutHumanraceContainer.style.display = "none";
+window.onscroll = () => {
+    let top = window.scrollY;
+    if (top >= 50) {
+        navBackground.classList.add("active")
+    } else {
+        navBackground.classList.remove("active")
+    }
+}
 
-// if (aboutHumanraceContainer.style.display = 'none') {
-//     aboutHumanraceBtn.addEventListener('mouseover', () => {
-//     aboutHumanraceContainer.style.display = "flex";
-// })
-// } if (aboutHumanraceContainer.style.display = "flex") {
-//     aboutHumanraceBtn.addEventListener('mouseout', () => {
-//     aboutHumanraceContainer.style.display = "none";
-// })
-// }
+const ahrBtn = document.querySelector(".about-humanrace")
+const ahrContainer = document.querySelector(".about-humanrace-container")
+
+ahrBtn.addEventListener("mouseover", () => {
+    ahrContainer.classList.add("active")
+    navBackground.classList.add("active")
+
+    if (ahrContainer.classList.contains("active")) {
+        ahrContainer.addEventListener("mouseout", () => {
+            ahrContainer.classList.remove("active")
+            navBackground.classList.remove("active")
+        })
+    }
+})
+
+const burger = document.querySelector(".burger")
+const mobileMenu = document.querySelector("#mobile-menu")
+const setsandkits = document.querySelector("#setsandkits")
+
+
+let toggleStatus = 1;
+function toggleMenu() {
+    if (toggleStatus == 1) { 
+        mobileMenu.style.display = "flex"
+         toggleStatus = 0;
+    } else if (toggleStatus == 0) {
+        toggleStatus = 1;
+        mobileMenu.style.display = "none"
+    }
+}
